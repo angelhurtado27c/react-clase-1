94bea8eba4c4b0275abee4a3d6bac1d042a506b6
Ángel Hurtado <angelhurtado27c@gmail.com>
2025-01-15 10:40:41 -0500

    Experimentando con JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import './App.css'
+import img_react from './logo.svg'
 
 function App() {
-  return (
-    <div className="mi-clase">
-      <h1>Hola mundo</h1>
-    </div>
-  );
+  let nombre = 'Ángel Hurtado'
+  let profesion = 'Programador web'
+  const tarjeta = <div className="tarjeta-profesion">
+    <h2>{nombre}</h2>
+    <p>{profesion}</p>
+  </div>
+
+  return <div className="mi-clase">
+    <h1>Hola mundo desde React !!!</h1>
+    {tarjeta}
+    <img src={img_react} className="logo"/>
+    {{a:"asdf", b:"asdffd"}["b"]}
+  </div>
 }
 
 export default App;
